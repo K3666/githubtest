5ecf30b0d796913fd62400f6555de1f4e0800e2c
K3666 <68623052+K3666@users.noreply.github.com>
2020-12-29 22:37:17 +0100

    Create solution-34.py

---
+++ solution-34.py
@@ -0,0 +1,8 @@
+def my_find_sum_sample_(sample_list):
+    # At first initialize total_sum to 0
+    my_sum = 0
+    # Iterate through the list
+    for item in sample_list:
+        # Add each element to my_sum
+        my_sum = my_sum + item
+    return my_sum
